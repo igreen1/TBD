a463fb3129fc5395d2ee12911f40c3715077e134
igreen1 <iangreen2.00@gmail.com>
2020-11-11 17:08:21 -0800

    Added textbox and newmessage page

---
+++ determined-mail/src/components/NewMessage.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import TextBox from './Textbox';
+import PropTypes from 'prop-types';
+
+class NewMesssage extends Component{
+
+
+    render(){
+        return (
+            <div>
+                <TextBox className="To" singleLine="true" title="To"/>
+                <TextBox className="CC" singleLine="true" title="CC"/>
+                <TextBox className="BCC" singleLine="true" title="BCC" />
+                <TextBox className="EmailInput" defaultText="Type here"/>
+            </div>
+            )
+    }
+
+}
+
+export default NewMesssage;
